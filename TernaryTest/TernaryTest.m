794223b0f86b606a3cf10f25233d1a9f0ddcb57d
Gaku Ueda <gaku@gaku.net>
2012-12-23 21:25:48 -0800

    - Introduced GKTernaryTreeItem protocol. - It can store items. - Introduced retrieve method.

--- TernaryTest/TernaryTest.m
+++ TernaryTest/TernaryTest.m
@@ -9,6 +9,27 @@
 #import "TernaryTest.h"
 #import "GKTernaryNode.h"
 
+@implementation StringItem
+- (StringItem*)initWithString:(NSString*)str
+{
+    self = [super init];
+    if (self) {
+        self.str = str;
+    }
+    return self;
+}
+
+- (NSString*)stringValue
+{
+    return self.str;
+}
+
+- (NSString*)description
+{
+    return self.str;
+}
+@end
+
 @implementation TernaryTest
 
 - (void)setUp
@@ -27,34 +48,75 @@
 
 - (void)testInsert
 {
-    GKTernaryNode* node = [[GKTernaryNode alloc] init];
-    [node insert:@"hello"];
+    GKTernarySearchTree* tree = [[GKTernarySearchTree alloc] init];
+    [tree insert:[[StringItem alloc] initWithString:@"hello"]];
     
-    STAssertEquals(YES, [node search:@"hello"], @"an inserted item should be found");
-    STAssertEquals(YES, [node search:@"hell"], @"substrings should be found");
-    STAssertEquals(NO, [node search:@"helo"], @"this doesn't exist in the tree");
+    STAssertEquals(YES, [tree search:@"hello"], @"an inserted item should be found");
+    STAssertEquals(YES, [tree search:@"hell"], @"substrings should be found");
+    STAssertEquals(NO, [tree search:@"helo"], @"this doesn't exist in the tree");
 }
 
 - (void)testInsertTwo
 {
-    GKTernaryNode* node = [[GKTernaryNode alloc] init];
-    [node insert:@"ab"];
-    [node insert:@"aa"];
-    [node insert:@"ac"];
+    GKTernarySearchTree* tree = [[GKTernarySearchTree alloc] init];
 
-    STAssertEquals(YES, [node search:@"aa"], @"aa should be found");
-    STAssertEquals(YES, [node search:@"ab"], @"ab should be found");
-    STAssertEquals(YES, [node search:@"ac"], @"ac should be found");
+    [tree insert:[[StringItem alloc] initWithString:@"ab"]];
+    [tree insert:[[StringItem alloc] initWithString:@"aa"]];
+    [tree insert:[[StringItem alloc] initWithString:@"ac"]];
+
+    STAssertEquals(YES, [tree search:@"aa"], @"aa should be found");
+    STAssertEquals(YES, [tree search:@"ab"], @"ab should be found");
+    STAssertEquals(YES, [tree search:@"ac"], @"ac should be found");
 }
 
 - (void)testUTF8
 {
-    GKTernaryNode* node = [[GKTernaryNode alloc] init];
-    [node insert:@"あか"];
-    [node insert:@"あお"];
+    GKTernarySearchTree* tree = [[GKTernarySearchTree alloc] init];
+
+    [tree insert:[[StringItem alloc] initWithString:@"あか"]];
+    [tree insert:[[StringItem alloc] initWithString:@"あお"]];
     
-    STAssertEquals(YES, [node search:@"あか"], @"あか should be found");
-    STAssertEquals(YES, [node search:@"あお"], @"あお should be found");
-    STAssertEquals(NO, [node search:@"あき"], @"あき should not be found");
+    STAssertEquals(YES, [tree search:@"あか"], @"あか should be found");
+    STAssertEquals(YES, [tree search:@"あお"], @"あお should be found");
+    STAssertEquals(NO, [tree search:@"あき"], @"あき should not be found");
+}
+
+- (void)testRetrieval
+{
+    GKTernarySearchTree* tree = [[GKTernarySearchTree alloc] init];
+    
+    StringItem* i111 = [[StringItem alloc] initWithString:@"111"];
+    StringItem* i123 = [[StringItem alloc] initWithString:@"123"];
+    StringItem* i124 = [[StringItem alloc] initWithString:@"124"];
+    StringItem* i130_1 = [[StringItem alloc] initWithString:@"130"];
+    StringItem* i130_2 = [[StringItem alloc] initWithString:@"130"];  // Same string representation but different object
+
+    [tree insert:i124];
+    [tree insert:i111];
+    [tree insert:i123];
+    [tree insert:i130_1];
+    [tree insert:i130_2];
+    
+    NSArray* output = [tree retrieve:@"123"];
+    NSLog(@"length:%d, %@", [output count], output);
+    STAssertEquals(1, (int)[output count], @"expects ['123']");
+    STAssertEquals(YES, [output containsObject:i123], @"123");
+    
+    output = [tree retrieve:@"12"];
+    NSLog(@"length:%d, %@", [output count], output);
+    STAssertEquals(2, (int)[output count], @"expects ['123', '124']");
+    STAssertEquals(YES, [output containsObject:i123], @"123");
+    STAssertEquals(YES, [output containsObject:i124], @"124");
+
+    output = [tree retrieve:@"11"];
+    NSLog(@"length:%d, %@", [output count], output);
+    STAssertEquals(1, (int)[output count], @"expects ['111']");
+    STAssertEquals(YES, [output containsObject:i111], @"111");
+
+    output = [tree retrieve:@"130"];
+    NSLog(@"length:%d, %@", [output count], output);
+    STAssertEquals(2, (int)[output count], @"expects ['130','130']");
+    STAssertEquals(YES, [output containsObject:i130_1], @"130_1");
+    STAssertEquals(YES, [output containsObject:i130_2], @"130_2");
 }
 @end
